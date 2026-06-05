ae838ebc6b1f3f027a65ea31f3bb9d962f5efa23
Vladislav Petrov <vladislav.petrov.08@gmail.com>
2026-06-05 17:32:09 +0300

    start|fin

---
+++ app/notes/page.tsx
@@ -0,0 +1,19 @@
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { getQueryClient } from '../../lib/getQueryClient';
+import { fetchNotes } from '../../lib/api';
+import NotesClient from './Notes.client';
+
+export default async function NotesPage() {
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['notes', 1, ''],
+    queryFn: () => fetchNotes(1, 12, ''),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient />
+    </HydrationBoundary>
+  );
+}
